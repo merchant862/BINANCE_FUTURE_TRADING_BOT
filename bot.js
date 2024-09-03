a8a7a3bf0a88382b7aa92cb8ea0a299678ca253b
blackhole <blackhole@blackhole.blackhole>
2024-09-04 01:47:05 +0500

    added cli arguments + pm2 stuff for bot

--- bot.js
+++ bot.js
@@ -1,99 +1,17 @@
-const schedule = require('node-schedule');
 const { exec } = require('child_process');
-
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
-
-const fifteenMinChartMonitoringInEveryThreeMinutes = require('./trading_strategies/bollinger/fifteenMinChartMonitoringInEveryThreeMinutes');
-const binanceConfig = require('./utils/SDKs/BINANCE/sdk');
-
-eventEmitter.on('rerun', async() => 
-{
-    setTimeout((async() => 
-    {
-        await tradingBot()
-    }), 180)
-});
-
-const tradingBot = async () => 
-{
-    try
-    {
-        let moniterMarket = await fifteenMinChartMonitoringInEveryThreeMinutes(
-        {
-            symbol: 'BTCUSDT',
-            interval: '15m',
-            klineLimit: 100,
-            period: 20,
-            stdDev: 2
-        });
-    
-        if(!moniterMarket || moniterMarket == false) 
-        {
-            console.log('No buying opportunity!\n Re-running thr bot!');
-            eventEmitter.emit('rerun');
-        }
-
-        else
-        {
-            await binanceConfig.setLeverage(
-            {
-                symbol: 'BTCUSDT',
-                leverage: 20,
-                timestamp: Date.now(),
-            });
-            
-            let userBalance = await binanceConfig.getBalance();
-            let userUSDTBalance = userBalance.find(item => item.asset === 'BTC');
-
-            if(!userUSDTBalance.availableBalance || userUSDTBalance.availableBalance == 0 || userUSDTBalance.availableBalance == '0.00000000')
-            {
-                console.log('Not enough balance!\n Re-running thr bot!');
-                eventEmitter.emit('rerun');
-            }
- 
-            else
-            {
-                let coinPrice = await binanceConfig.marketPriceOfACoin({ symbol: 'BTCUSDT' });
-                let quantitytoSpend = parseFloat(((parseFloat(userUSDTBalance.availableBalance) / 100) * 1) / parseFloat(coinPrice)).toFixed(2);
-                
-                let placetrade = await binanceConfig.placeTrade(
-                {
-                    symbol: 'BTCUSDT',
-                    side: 'BUY',
-                    positionSide: 'LONG',
-                    type: 'MARKET', 
-                    quantity: quantitytoSpend,
-                    recvWindow: '20000',
-                    timestamp: Date.now(),
-                });
-
-                console.log(placetrade)
-            }
-        }
-    }
-
-    catch(error)
-    {
-        console.error(`${error}\n Re-running thr bot!`);
-        eventEmitter.emit('rerun');
-    }
-};
 
 try
 {
-    let SCRIPT = async() => 
+    const shellCommand = `pm2 start trading_strategies/bollinger/fifteenMinChartMonitoringInEveryThreeMinutes.js --name "fifteenMinChartMonitoring" -- --leverage=20 --symbol=BTCUSDT --interval=15m --klineLimit=100 --period=20 --stdDev=2 --tradeSide=BUY --asset=BTC --tradePosition=LONG --orderType=MARKET --strategyInterval=180`;
+                    
+    exec(shellCommand, (error, stdout, stderr) => 
     {
-        //setInterval(scrap, CONFIG.lander_to_lander_delay);
-        await tradingBot();
-    }
-
-    SCRIPT();
+        if (error) console.error(error);
+        else console.log('Script running!');
+    });
 }
 
 catch(error)
 {
     console.error(error);
 }
-
-module.exports = tradingBot;
--- trading_strategies/bollinger/fifteenMinChartMonitoringInEveryThreeMinutes.js
+++ trading_strategies/bollinger/fifteenMinChartMonitoringInEveryThreeMinutes.js
@@ -1,36 +1,125 @@
+const args = require('minimist')(process.argv.slice(2));
+
 let binanceSDK              = require('../../utils/SDKs/BINANCE/sdk');
 let calculateBollingerBands = require('../../utils/calculateBollinger');
 
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('rerun', async(data) => 
+{
+    setTimeout((async() => 
+    {
+        await fifteenMinChartMonitoringInEveryThreeMinutes(data)
+    }), data.strategyInterval/* 180000 */)
+});
+
 async function fifteenMinChartMonitoringInEveryThreeMinutes(data)
 {
-    let klineData = await binanceSDK.klineData(
+    try
     {
-        symbol: data.symbol,
-        interval: data.interval,
-        limit: data.klineLimit
-    });
-
-    let closePrices = klineData.map(candle => parseFloat(candle.close)).filter(price => !isNaN(price));
-
-    let BB = await calculateBollingerBands(
-    {
-        prices: closePrices,
-        period: data.period,
-        stdDev: data.stdDev
-    });
-
-    let bollingerLowest = BB.reduce((min, item) => Math.min(min, item.lower), Infinity);
-
-    let marketPriceOfACoin = await binanceSDK.marketPriceOfACoin({ symbol: data.symbol });
-
-    let percentageDifference = Math.abs((marketPriceOfACoin - bollingerLowest) / bollingerLowest) * 100;
+        let klineData = await binanceSDK.klineData(
+        {
+            symbol: data.symbol,
+            interval: data.interval,
+            limit: data.klineLimit
+        });
     
-    if (percentageDifference >= 0.01 && percentageDifference <= 1) 
-    {
-        return [percentageDifference, bollingerLowest, marketPriceOfACoin];
+        let closePrices = klineData.map(candle => parseFloat(candle.close)).filter(price => !isNaN(price));
+    
+        let BB = await calculateBollingerBands(
+        {
+            prices: closePrices,
+            period: data.period,
+            stdDev: data.stdDev
+        });
+    
+        let bollingerLowest = BB.reduce((min, item) => Math.min(min, item.lower), Infinity);
+    
+        let marketPriceOfACoin = await binanceSDK.marketPriceOfACoin({ symbol: data.symbol });
+    
+        let percentageDifference = Math.abs((marketPriceOfACoin - bollingerLowest) / bollingerLowest) * 100;
+        
+        if (percentageDifference >= 0.01 && percentageDifference <= 1) 
+        {
+            await binanceConfig.setLeverage(
+            {
+                symbol: data.symbol,
+                leverage: data.leverage,
+                timestamp: Date.now(),
+            });
+            
+            let userBalance = await binanceConfig.getBalance();
+            let userUSDTBalance = userBalance.find(item => item.asset === data.asset);
+    
+            if(!userUSDTBalance.availableBalance || userUSDTBalance.availableBalance == 0 || userUSDTBalance.availableBalance == '0.00000000')
+            {
+                console.log('Not enough balance!\n Re-running the bot!\n\n');
+                eventEmitter.emit('rerun', data);
+            }
+    
+            else
+            {
+                let quantitytoSpend = parseFloat(((parseFloat(userUSDTBalance.availableBalance) / 100) * 1) / parseFloat(marketPriceOfACoin)).toFixed(2);
+                
+                let placetrade = await binanceConfig.placeTrade(
+                {
+                    symbol: data.symbol,
+                    side: data.tradeSide, //'BUY',
+                    positionSide: data.tradePosition, //'LONG',
+                    type: data.orderType, //'MARKET', 
+                    quantity: quantitytoSpend,
+                    //recvWindow: '20000',
+                    timestamp: Date.now(),
+                });
+    
+                console.log(placetrade);
+                return [percentageDifference, bollingerLowest, marketPriceOfACoin];
+            }
+        }
+    
+        else 
+        {
+            console.log('No buying opportunity!\n Re-running the bot!\n\n');
+            eventEmitter.emit('rerun', data);
+        }
     }
 
-    else return false;
+    catch(error)
+    {
+        console.error(error);
+        eventEmitter.emit('rerun', data);
+    }
 }
 
-module.exports = fifteenMinChartMonitoringInEveryThreeMinutes;
+try
+{
+    let SCRIPT = async() => 
+    {
+        let data = 
+        {
+            leverage: args.leverage, //20
+            symbol: args.symbol, //'BTCUSDT',
+            asset: args.asset, //BTC
+            interval: args.interval, //'15m',
+            klineLimit: args.klineLimit, //100,
+            period: args.period, //20,
+            stdDev: args.stdDev, //2,
+            tradeSide: args.tradeSide, //'BUY',
+            tradePosition: args.tradePosition, //'LONG',
+            orderType: args.orderType, //'MARKET', 
+            strategyInterval: args.strategyInterval, //180
+        }
+        
+        await fifteenMinChartMonitoringInEveryThreeMinutes(data);
+    }
+
+    SCRIPT();
+}
+
+catch(error)
+{
+    console.error(error);
+}
+
+//module.exports = fifteenMinChartMonitoringInEveryThreeMinutes;
